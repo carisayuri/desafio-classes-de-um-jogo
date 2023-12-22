e539fbe3208b157b298d6b2117e70b8b9b3d94ae
carisayuri <carisayuri@gmail.com>
2023-12-22 14:14:59 -0300

    Resolver o desafio proposto pela DIO

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-//classes de um jogo
+class heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+    }
+    escrever(heroi) {
+        switch (heroi.tipo){
+            case "mago":
+            console.log("O " + this.tipo + " atacou usando magia.");
+            break;
+            case "guerreiro":
+                console.log("O " + this.tipo + " atacou usando espada.");
+            break;
+            case "monge":
+                console.log("O " + this.tipo + " atacou usando artes marciais.");
+            break;
+            case "ninja":
+            console.log("O " + this.tipo + " atacou usando shuriken.");
+            break;
+        }
+    }
+}
+
+let hero = new heroi("Leo", 36, "mago");
+
+heroi1.escrever(hero);
